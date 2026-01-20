c4d5988659495cc1a16a6603e69f703690026319
Lucas Sant <lucasfdesantana095@gmail.com>
2026-01-19 23:53:40 -0300

    feat: criando componente de criar conta

---
+++ finance-react/src/components/CreateAccount.jsx
@@ -0,0 +1,25 @@
+function CreateAccount(){
+    return (
+        <>
+        <main>
+            <div className='div-login'>
+                <div className='input-div'>
+                    <h4>Informe seu e-mail</h4>
+                    <input type="text" name='e-mail' />
+                </div>
+                <div className='input-div'>
+                    <h4>Informe sua senha</h4>
+                    <input type="password" />
+                    <p className='new-password'>esqueceu a senha?</p>
+                </div>
+                <div className='login-account'>
+                    <button className='login-request'>Acessar</button>
+                    <p className='new-account'>Criar conta</p>
+                </div>
+            </div>
+        </main>
+        </>
+    )
+}
+
+export default CreateAccount;
