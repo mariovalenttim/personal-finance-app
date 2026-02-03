4c07bea12434bd14fbac5009214b444c28bb3324
Lucas Sant <lucasfdesantana095@gmail.com>
2026-02-03 20:59:53 -0300

    feat: criacao de pasta de componente e antigos arquivos jsx

---
+++ finance-app/src/components/CreatedBy.jsx
@@ -0,0 +1,10 @@
+function CreatedBy(){
+    return <>
+        <footer>
+            2026 © - created by Igor Aurelino, Mário Valentim and Lucas Santana
+        </footer>
+    
+    </>
+}
+
+export default CreatedBy;
